d4ba6b4cc74752a0a812e8f0e5daf7f86d27b667
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-07 21:35:54 +0200

    Add Base Entity

---
+++ CRM.Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CRM.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int BaseEntityId { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
