73492f75d83ac51c7f50f03d66034289d47068ee
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-08 19:05:26 +0200

    Added generic IRepository interface

---
+++ CRM.Core/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CRM.Core.Entities
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(long id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Remove(T entity);
+        void SaveChanges();
+    }
+}
