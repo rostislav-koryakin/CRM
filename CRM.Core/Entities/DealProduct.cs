a9d0222ab2888dc274465b24a63f4c5cec8f50b1
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-10 22:52:33 +0200

    Updated DealProduct Entity

--- CRM.Core/Entities/DealProduct.cs
+++ CRM.Core/Entities/DealProduct.cs
@@ -2,9 +2,10 @@
 {
     public class DealProduct : BaseEntity
     {
+        public int Id { get; set; }
         public int DealId { get; set; }
         public Deal Deal { get; set; }
-        public int ServiceId { get; set; }
+        public int ProductId { get; set; }
         public Product Product { get; set; }
     }
 }
