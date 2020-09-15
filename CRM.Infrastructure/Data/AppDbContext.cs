9d1a71643df4df6b6c3131f05e12c3a683129ab5
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-15 20:50:36 +0200

    Added AppDbContext class

---
+++ CRM.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CRM.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CRM.Infrastructure.Data
+{
+    class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<BaseEntity> BaseEntities { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Deal> Deals { get; set; }
+        public DbSet<DealProduct> DealProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Salesman> Salesmen { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
