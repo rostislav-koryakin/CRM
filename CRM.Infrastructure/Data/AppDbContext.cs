d60498818cebdde253dbf8d90a907189e60a1c83
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-21 21:02:41 +0200

    Overriding SaveChanges and setting Created and Modified Date

--- CRM.Infrastructure/Data/AppDbContext.cs
+++ CRM.Infrastructure/Data/AppDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using CRM.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using System.Reflection;
 
 namespace CRM.Infrastructure.Data
@@ -22,6 +24,31 @@ namespace CRM.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override int SaveChanges()
+        {
+            SetBaseProperties();
+            return base.SaveChanges();
+        }
+
+        public void SetBaseProperties()
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is BaseEntity
+                    && (e.State == EntityState.Added
+                    || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                ((BaseEntity)entityEntry.Entity).UpdatedDate = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((BaseEntity)entityEntry.Entity).CreatedDate = DateTime.Now;
+                }
+            }
         }
     }
 }
