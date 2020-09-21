9d6b433190d8f1ce01412e651823a24362fabbb1
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-21 21:06:22 +0200

    Added EfRepository

---
+++ CRM.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,52 @@
+﻿using CRM.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRM.Infrastructure.Data
+{
+    class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _dbContext;
+        private DbSet<T> _entities;
+        
+        public EfRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _entities = _dbContext.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return _entities.SingleOrDefault(e => e.Id == id);
+        }
+
+        public void Add(T entity)
+        {
+            _entities.Add(entity);
+            SaveChanges();
+
+        }
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            SaveChanges();
+
+        }
+        public void Delete(T entity)
+        {
+            _entities.Remove(entity);
+            SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
