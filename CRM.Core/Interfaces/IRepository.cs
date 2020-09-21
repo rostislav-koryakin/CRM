4920111c4b1a1afa29f158a51ed8d22c069e23ec
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-21 20:47:32 +0200

    Update IRepository

--- CRM.Core/Interfaces/IRepository.cs
+++ CRM.Core/Interfaces/IRepository.cs
@@ -1,17 +1,14 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace CRM.Core.Entities
 {
     public interface IRepository<T> where T : BaseEntity
     {
         IEnumerable<T> GetAll();
-        Task<T> GetByIdAsync(int id);
-        Task<T> AddAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(T entity);
-        void SetBaseProperties();
-        int SaveChanges();
-        void SaveChangesAsync();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
     }
 }
