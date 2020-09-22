166797dd163c2684995996dcadc51d5730448ef3
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-22 21:33:08 +0200

    Updated Context class

--- CRM.Infrastructure/Data/AppDbContext.cs
+++ CRM.Infrastructure/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 namespace CRM.Infrastructure.Data
 {
-    class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
