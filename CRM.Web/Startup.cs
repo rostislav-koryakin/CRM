2f8e5aebcc45515135422b8745aac173ea50b40f
Qba Fejkl <qba.fejkl@gmail.com>
2020-09-22 21:40:51 +0200

    Added DbContext configuration to services

--- CRM.Web/Startup.cs
+++ CRM.Web/Startup.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CRM.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +21,7 @@ namespace CRM.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer((Configuration.GetConnectionString("Default"))));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
