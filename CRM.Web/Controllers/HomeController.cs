e37e72f458719851271cbcdbaf0ae34955448696
Qba Fejkl <qba.fejkl@gmail.com>
2021-01-04 16:28:42 +0100

    Added new methods to get data for Homepage charts

--- CRM.Web/Controllers/HomeController.cs
+++ CRM.Web/Controllers/HomeController.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using CRM.Web.Models;
+using CRM.Web.ViewModels;
 using CRM.Infrastructure.Data;
+using Newtonsoft.Json.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRM.Web.Controllers
 {
@@ -27,14 +29,52 @@ namespace CRM.Web.Controllers
             return View();
         }
 
-        public IActionResult GetDeals()
+        public IActionResult GetDealsByStage()
         {
-            var deals = _context.Deals
-                .GroupBy(d => d.Stage)
-                .Select(g => new { stageName = g.Key.ToString(), stageTotalAmount = g.Sum(d => d.TotalAmount) });
+            var query = from d in _context.Deals
+                        group d by d.Stage into g
+                        where 
+                            g.Key == Core.Entities.Deal.DealStage.New || 
+                            g.Key == Core.Entities.Deal.DealStage.Ongoing ||
+                            g.Key == Core.Entities.Deal.DealStage.Won
+                        orderby g.Key
+                        select new
+                        {
+                            stageName = g.Key.ToString(),
+                            stageTotalAmount = g.Sum(d => d.TotalAmount)
+                        };
 
-            return new JsonResult(deals);
+            return new JsonResult(query);
         }
+
+        public IActionResult GetActivitiesBySalesman()
+        {
+            var query = from s in _context.Salesmen
+                        join a in _context.Activities
+                               on s.Id equals a.SalesmanId
+                        group a by a.Salesman.FirstName +" "+ a.Salesman.LastName into salesGroup
+                        orderby salesGroup.Count() descending
+                        select new
+                        {
+                            name = salesGroup.Key,
+                            sum = salesGroup.Count()
+                        };
+
+            //var query2 = _context.Salesmen
+            //        .Include(s => s.Activities)
+            //            .ThenInclude(s => s.Type)
+            //        .Select(s => new
+            //            {
+            //                name = s.LastName,
+            //                type = s.Activities
+            //            })
+            //        .ToList();
+
+            var result = query.Take(5);
+
+            return new JsonResult(result);
+        }
+
 
         public IActionResult Privacy()
         {
